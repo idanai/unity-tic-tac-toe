43d9826e576df75e5d38044de633833f4538a3b7
idany-papaya <idany@papaya.com>
2025-09-17 22:25:05 +0300

    Game Manager

---
+++ tic-tac-toe/Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+	[SerializeField] private GameBoardView _boardView;
+
+	private TicTacToeController _gameController;
+
+	private void Awake()
+	{
+	}
+
+	private void Start()
+	{
+		if (_gameController is not null)
+			return;
+
+		_gameController = new(_boardView, playerX: null, player0: new BadTicTacToeAi(), isXFirst: false);
+		UniTask.Create(async () =>
+		{
+			using (_gameController)
+			{
+				await _gameController.Play(destroyCancellationToken);
+				Debug.Log($"Game finished with state {_gameController.State}");
+			}
+			_gameController = null;
+		});
+	}
+}
