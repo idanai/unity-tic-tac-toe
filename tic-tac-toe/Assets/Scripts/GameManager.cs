8950e8101e890407812194f5d7d00fa2018e6222
idany-papaya <idany@papaya.com>
2025-09-17 23:48:50 +0300

    Menu with Replay button

--- tic-tac-toe/Assets/Scripts/GameManager.cs
+++ tic-tac-toe/Assets/Scripts/GameManager.cs
@@ -1,31 +1,45 @@
+using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
+public class GameManager : MonoBehaviour, GameMenu.IListener
 {
 	[SerializeField] private GameBoardView _boardView;
+	[SerializeField] private GameMenu _menu;
 
 	private TicTacToeController _gameController;
 
+	private void Awake()
+	{
+		_menu.Init(this);
+	}
+
 	private void Start()
 	{
+		Play(destroyCancellationToken).Forget();
+	}
+
+	private async UniTaskVoid Play(CancellationToken cancellationToken)
+	{
+		_menu.HideReplay();
 		if (_gameController is not null)
 			return;
 
 		_gameController = new(
 			boardView: _boardView,
-			playerX: null,
+			playerX: null, // null for human player
 			player0: new SimpleTicTacToeAi(1, 3),
 			isPlayerXFirst: Random.Range(0, 2) is 0);
 
-		UniTask.Create(async () =>
+		using (_gameController)
 		{
-			using (_gameController)
-			{
-				await _gameController.Play(destroyCancellationToken);
-				Debug.Log($"Game finished with state {_gameController.State}");
-			}
-			_gameController = null;
-		});
+			await _gameController.Play(cancellationToken);
+			Debug.Log($"Game finished with state {_gameController.State}");
+		}
+		_gameController = null;
+
+		_menu.ShowReplay();
 	}
+
+	void GameMenu.IListener.HandleReplayClick() => Play(destroyCancellationToken).Forget();
 }
---
+++ tic-tac-toe/Assets/Scripts/GameMenu.cs
@@ -0,0 +1,25 @@
+using JetBrains.Annotations;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameMenu : MonoBehaviour
+{
+	public interface IListener
+	{
+		void HandleReplayClick();
+	}
+
+	[SerializeField] private Button _replayButton;
+
+	private IListener _listener;
+
+
+	public void Init(IListener listener) => _listener = listener;
+
+	public void ShowReplay() => _replayButton.gameObject.SetActive(true);
+
+	public void HideReplay() => _replayButton.gameObject.SetActive(false);
+
+	[UsedImplicitly]
+	public void OnReplayClick() => _listener.HandleReplayClick();
+}
