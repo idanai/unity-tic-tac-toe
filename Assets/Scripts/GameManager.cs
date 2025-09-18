a7da0ad3ca616bfdacc8635a4faa43166fcff379
idanai <idany@tutanota.com>
2025-09-18 18:45:19 +0300

    implement most of IGameManager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,45 +1,78 @@
+using System;
 using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour, GameMenu.IListener
+public class GameManager : MonoBehaviour, PlayPerfect.IGameManager, GameMenu.IListener
 {
 	[SerializeField] private GameBoardView _boardView;
 	[SerializeField] private GameMenu _menu;
 
 	private TicTacToeController _gameController;
+	private CancellationTokenSource _runningGameCts;
 
-	private void Awake()
+	public event Action OnGameOver;
+
+	// Note: the assignment defines the player as always playing X
+	public UniTask WaitForPlayerTurn()
+		=> _gameController.WaitForPlayerXTurnOrEnd().AttachExternalCancellation(destroyCancellationToken);
+
+	public bool IsGameInProgress => _gameController?.IsFinished is true;
+
+	public int GetFinalScore()
 	{
-		_menu.Init(this);
+		throw new NotImplementedException(); // TODO
 	}
 
-	private void Start()
+	public UniTask LoadNewGameAsync(bool? isUserFirstTurn = null)
 	{
-		Play(destroyCancellationToken).Forget();
+		CancelRunningGame(new());
+		return Play(isUserFirstTurn, destroyCancellationToken);
 	}
 
-	private async UniTaskVoid Play(CancellationToken cancellationToken)
+	private void CancelRunningGame(CancellationTokenSource newCts)
 	{
-		_menu.HideReplay();
+		if (_runningGameCts is null)
+			return;
+
+		_runningGameCts.Cancel();
+		_runningGameCts.Dispose();
+		_runningGameCts = newCts;
+	}
+
+	private async UniTask Play(bool? isPlayerXFirst, CancellationToken cancellationToken)
+	{
 		if (_gameController is not null)
 			return;
+
+		_menu.HideReplay();
 
 		_gameController = new(
 			boardView: _boardView,
 			playerX: null, // null for human player
 			player0: new SimpleTicTacToeAi(1, 3),
-			isPlayerXFirst: Random.Range(0, 2) is 0);
+			isPlayerXFirst: isPlayerXFirst ?? UnityEngine.Random.Range(0, 2) is 0); 
 
 		using (_gameController)
 		{
 			await _gameController.Play(cancellationToken);
 			Debug.Log($"Game finished with state {_gameController.State}");
+			OnGameOver?.Invoke();
 		}
 		_gameController = null;
 
 		_menu.ShowReplay();
 	}
 
-	void GameMenu.IListener.HandleReplayClick() => Play(destroyCancellationToken).Forget();
+	void GameMenu.IListener.HandleReplayClick() => Play(null, destroyCancellationToken).Forget();
+
+	private void Awake() => _menu.Init(this);
+
+	private void OnDestroy() => CancelRunningGame(null);
+
+	private void Start()
+	{
+		CancelRunningGame(new());
+		Play(null, destroyCancellationToken).Forget();
+	}
 }
--- Assets/Scripts/TicTacToeController.cs
+++ Assets/Scripts/TicTacToeController.cs
@@ -1,24 +1,36 @@
 using System;
 using System.Threading;
+using System.Threading.Tasks;
 using Cysharp.Threading.Tasks;
 using JetBrains.Annotations;
 using UnityEngine;
 
 public class TicTacToeController : GameTileView.IListener, IDisposable
 {
-	public enum GameLoopState : byte { Start, Continue, X, O, Draw };
+	public enum GameLoopState : byte
+	{
+		Start,
+		Continue,
+		WinX,
+		WinO,
+		Draw,
+	};
 
 	private readonly GameBoardView _boardView;
 	[CanBeNull] private readonly ITicTacToeAi _playerX;
 	[CanBeNull] private readonly ITicTacToeAi _player0;
+
 	// state
 	private readonly GameBoard _board = new();
 	private readonly CancellationTokenSource _cts = new();
 	private UniTaskCompletionSource<Vector2Int> _userInputTcs;
+	private UniTaskCompletionSource<GameBoard.TileState> _currentPlayerTurnTcs;
 	private byte _turn; // up to 9 turns
 	private readonly bool _isPlayerXFirst;
 
 	public GameLoopState State { get; private set; } = GameLoopState.Start;
+
+	public bool IsFinished => State is not (GameLoopState.Start or GameLoopState.Continue);
 
 	private bool IsPlayerXTurn => _turn % 2 == (_isPlayerXFirst ? 1 : 0);
 
@@ -27,8 +39,12 @@ public class TicTacToeController : GameTileView.IListener, IDisposable
 
 	/// <param name="playerX">AI, or null for player</param>
 	/// <param name="player0">AI, or null for player</param>
-	public TicTacToeController(GameBoardView boardView, [CanBeNull] ITicTacToeAi playerX, [CanBeNull] ITicTacToeAi player0, bool isPlayerXFirst)
-	{
+	public TicTacToeController(
+		GameBoardView boardView,
+		[CanBeNull] ITicTacToeAi playerX,
+		[CanBeNull] ITicTacToeAi player0,
+		bool isPlayerXFirst
+	) {
 		_boardView = boardView;
 		_playerX = playerX;
 		_player0 = player0;
@@ -37,8 +53,18 @@ public class TicTacToeController : GameTileView.IListener, IDisposable
 		boardView.Init(this);
 	}
 
+	public UniTask WaitForPlayerXTurnOrEnd() => WaitForPlayerTurnOrEnd(GameBoard.TileState.X);
+
+	public UniTask WaitForPlayerOTurnOrEnd() => WaitForPlayerTurnOrEnd(GameBoard.TileState.O);
+
+	private async UniTask WaitForPlayerTurnOrEnd(GameBoard.TileState tileState)
+	{
+		while (!IsFinished && await _currentPlayerTurnTcs.Task != tileState) {}
+	}
+
 	public void Dispose()
 	{
+		SignalEndGame();
 		_userInputTcs?.TrySetCanceled();
 		_cts.Cancel();
 		_cts.Dispose();
@@ -47,15 +73,20 @@ public class TicTacToeController : GameTileView.IListener, IDisposable
 	/// <summary>Starts a new game. DO NOT CALL MORE THAN ONCE on the same instance</summary>
 	public async UniTask Play(CancellationToken cancellationToken = default)
 	{
+		var lifetimeToken = _cts.Token;
+
 		if (State is not GameLoopState.Start)
 			throw new InvalidOperationException("Game already started");
 
-		using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, _cts.Token);
+		using var cts = CancellationTokenSource.CreateLinkedTokenSource(cancellationToken, lifetimeToken);
+
 		do
 		{
 			State = await IterateGameLoop(cts.Token);
 			Debug.Log($"Turn = {_turn}");
 		} while (State is GameLoopState.Continue);
+
+		SignalEndGame();
 	}
 
 	private async UniTask<GameLoopState> IterateGameLoop(CancellationToken cancellationToken)
@@ -65,19 +96,25 @@ public class TicTacToeController : GameTileView.IListener, IDisposable
 		if (_board.IsWin(out var winShape, out var winner, out var index))
 		{
 			_boardView.ShowStrikeLine(winShape, index);
-			return winner is GameBoard.TileState.X ? GameLoopState.X : GameLoopState.O;
+			return winner is GameBoard.TileState.X ? GameLoopState.WinX : GameLoopState.WinO;
 		}
 
+		// out of turns, it's a draw
 		if (_turn is GameBoard.AREA)
-		{
-			// out of turns, it's a draw
 			return GameLoopState.Draw;
-		}
 
 		_turn++;
 
 		var (ai, tileState) = CurrentPlayerInfo;
+		await HandlePlayerTurn(ai, tileState, cancellationToken);
+		_currentPlayerTurnTcs?.TrySetResult(tileState);
+		_currentPlayerTurnTcs = new();
 
+		return GameLoopState.Continue;
+	}
+
+	private async UniTask HandlePlayerTurn(ITicTacToeAi ai, GameBoard.TileState tileState, CancellationToken cancellationToken)
+	{
 		Vector2Int pos;
 		if (ai is null)
 		{
@@ -91,8 +128,8 @@ public class TicTacToeController : GameTileView.IListener, IDisposable
 			pos = ai.ChooseMove(_board);
 			await delay;
 		}
+
 		SetTile(pos, tileState);
-		return GameLoopState.Continue;
 	}
 
 	void GameTileView.IListener.HandleClick(GameTileView tile)
@@ -122,7 +159,8 @@ public class TicTacToeController : GameTileView.IListener, IDisposable
 		// should always succeed since TryGetCell worked
 		if (!_board.TrySetTile(GameBoard.TileState.X, pos.x, pos.y))
 		{
-			throw new ArithmeticException($"{nameof(_board.TryGetTile)} and {nameof(_board.TrySetTile)} disagree on bounds at {pos}");
+			throw new ArithmeticException(
+				$"{nameof(_board.TryGetTile)} and {nameof(_board.TrySetTile)} disagree on bounds at {pos}");
 		}
 
 		_userInputTcs.TrySetResult(pos);
@@ -132,5 +170,11 @@ public class TicTacToeController : GameTileView.IListener, IDisposable
 	{
 		_board.TrySetTile(value, pos.x, pos.y);
 		_boardView.SetTile(value, pos.x, pos.y);
+	}
+
+	private void SignalEndGame()
+	{
+		_currentPlayerTurnTcs?.TrySetResult(GameBoard.TileState.Empty);
+		_currentPlayerTurnTcs = null;
 	}
 }
